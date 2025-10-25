8d9b8914b2b3ddc5458d4d4b2cbaaae34d7e1861
Pralay Pal <pralaypal111@gmail.com>
2025-10-26 00:13:00 +0530

    ok

--- app.js
+++ app.js
@@ -88,6 +88,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res) => {
+  res.redirect("/listing");
+});
+
 //------------------ Routes ------------------
 app.use("/", userRouter);
 app.use("/listing", listingRouter);
